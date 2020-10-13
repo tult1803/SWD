2d8ac38c433cd1d9e996d799d1a2eb83aec70c51
DatVTP <vanthanhphuongdat1309@gmai.com>
2020-10-13 15:40:55 +0700

    Add Store ID to QR Code

--- qr_code_example-master/lib/main.dart
+++ qr_code_example-master/lib/main.dart
@@ -34,41 +34,16 @@ Timer timer;
 class HomePageState extends State {
   String urlImg = 'images/img01.png';
    String data = "12345680";
-
+   String storeId = '1';
   @override
   void initState() {
-    var rand = Random();
     timer = Timer.periodic(Duration(seconds: 5), (time) async{
       DateTime now = DateTime.now();
       String formattedDate = DateFormat('yyyy-MM-dd – kk:mm:ss').format(now);
       setState(() {
-        int count = rand.nextInt(10);
-        print("Count: $count");
-        if(count == 0){
-          urlImg = 'images/img01.png';
-        }else if(count == 1){
-          urlImg = 'images/img03.jpeg';
-        }else if(count == 2){
-          urlImg = 'images/img03.jpeg';
-        }else if(count == 3){
-          urlImg = 'images/img04.png';
-        }else if(count == 4){
-          urlImg = 'images/img05.jpeg';
-        }else if(count == 5){
-          urlImg = 'images/img06.jpeg';
-        }else if(count == 6){
-          urlImg = 'images/img07.jpeg';
-        }else if(count == 7){
-          urlImg = 'images/img08.jpeg';
-        }else if(count == 8){
-          urlImg = 'images/img09.jpeg';
-        }else if(count == 9){
-          urlImg = 'images/img10.jpeg';
-        }
-        data = '${formattedDate}';
+        data = '${formattedDate};${storeId}';
       });
-        print(data);
-
+      print(data);
     });
   }
 
@@ -79,25 +54,20 @@ class HomePageState extends State {
 //        title: Text("QRCode"),
 //      ),
       body:  Container(
-        decoration: BoxDecoration(
-            image: DecorationImage(
-                fit: BoxFit.fill,
-                image: AssetImage(urlImg)
-            )
+        child: Center(
+          child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                SizedBox(height: 16,),
+                QrImage(
+                  data: data,
+                  gapless: true,
+                  size: 350,
+                  errorCorrectionLevel: QrErrorCorrectLevel.H,
+                )
+              ],
+            ),
         ),
-        alignment: Alignment.bottomRight,
-        child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: <Widget>[
-              SizedBox(height: 16,),
-              QrImage(
-                data: data,
-                gapless: true,
-                size: 150,
-                errorCorrectionLevel: QrErrorCorrectLevel.H,
-              )
-            ],
-          ),
       ),
     );
   }
