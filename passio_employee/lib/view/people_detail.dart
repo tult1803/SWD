61048d71419244d3e08909f133e08e3f78c58d8d
Anh Dung <littlemozarthver23@gmail.com>
2020-11-04 01:53:44 +0700

    Create people detail page

--- passio_employee/lib/view/people_detail.dart
+++ passio_employee/lib/view/people_detail.dart
@@ -1,23 +1,25 @@
+import 'package:passioemployee/model/model_profile_emp.dart';
 import 'package:passioemployee/model/url/url_color.dart';
 import 'package:flutter/material.dart';
 import 'package:passioemployee/model/model_news.dart';
 
-class NewsDetails extends StatefulWidget {
-  final DataNews dataNews;
+class PeopleDetails extends StatefulWidget {
+  final DataProfile dataPeople;
 
   // NewsDetails(this.id);
-  NewsDetails({Key key, @required this.dataNews}) : super(key: key);
+  PeopleDetails({Key key, @required this.dataPeople}) : super(key: key);
 
   @override
-  DetailNewsState createState() => DetailNewsState(dataNews);
+  DetailPeopleState createState() => DetailPeopleState(dataPeople);
 }
 
-class DetailNewsState extends State<NewsDetails> {
-  final DataNews dataNews;
-  DataNews data;
-  String news_id, title, content, createDate, authorId;
+class DetailPeopleState extends State<PeopleDetails> {
+  final DataProfile dataPeople;
+  DataProfile data;
+  String id, main_store_id, address, email, phone_number, bank_number, name, salary;
+  bool gender, active;
 
-  DetailNewsState(this.dataNews);
+  DetailPeopleState(this.dataPeople);
 
   @override
   void initState() {
@@ -27,13 +29,18 @@ class DetailNewsState extends State<NewsDetails> {
   }
 
   void _getToken() async {
-    data = dataNews;
+    data = dataPeople;
     setState(() {
-      news_id = data.id.toString();
-      title = data.decription;
-      content = data.content_html;
-      authorId = data.author_id.toString();
-      createDate = '${data.date_create.substring(0, 10)}';
+      id = data.id.toString();
+      main_store_id = data.main_store_id.toString();
+      address = data.address;
+      email = data.email;
+      phone_number = data.phone_number;
+      bank_number = data.bank_number;
+      name = data.name;
+      salary = data.salary.toString();
+      // gender
+      // active
     });
     // print('${data.keys}');
   }
@@ -53,7 +60,7 @@ class DetailNewsState extends State<NewsDetails> {
         ),
         backgroundColor: color_main,
         title: Text(
-          "Chi tiết thông tin",
+          "Thông tin nhân viên",
           style: TextStyle(color: Colors.black),
         ),
       ),
@@ -67,30 +74,36 @@ class DetailNewsState extends State<NewsDetails> {
             // height: 200,
             width: size.width,
             child: Card(
-              clipBehavior: Clip.antiAlias,
+              color: Colors.white,
+              shape: RoundedRectangleBorder(
+                side: BorderSide(color: Colors.black12, width: 1),
+                borderRadius: BorderRadius.circular(10),
+              ),
               child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  ListTile(
-                    // leading: Icon(Icons.arrow_drop_down_circle),
-                    title: Text('$title', style: TextStyle(fontWeight: FontWeight.bold),),
-                    subtitle: Text(
-                      'Ngày đăng: $createDate',
-                      style: TextStyle(color: Colors.black.withOpacity(0.6)),
-                    ),
+                  SizedBox(
+                    height: 10,
                   ),
-                  Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: Text(
-                      '$content',
-                      style: TextStyle(color: Colors.black.withOpacity(0.6)),
+                  card('Email', '$email'),
+                  card('Tên nhân viên', '$name'),
+                  Row(children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        card('Địa chỉ', '$address'),
+                        card('Số điện thoại', '$phone_number'),
+                        card('Lương', '$salary' + ' VNĐ'),
+                      ],
                     ),
-                  )
-                  // Image.asset('assets/card-sample-image-2.jpg'),
+                  ]),
+                  SizedBox(
+                    height: 20,
+                  ),
                 ],
               ),
             ),
-            ),
-          // ),
+          ),
         ],
       ),
     );
